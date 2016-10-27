d5fed54239c3dcec0db189754529d38b8fab495c
mark breneman <mark.breneman@gmail.com>
2016-10-27 14:51:03 -0400

    rerack to PWM with NPN Transistor.

--- Curve-ID-Lighting.ino
+++ Curve-ID-Lighting.ino
@@ -1,69 +1,28 @@
-//LEDSTRIP SETUP 
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+//////////////////////////////////////////////////////////////////
+//©2011 bildr
+//Released under the MIT License - Please reuse change and share
+//Simple code to output a PWM sine wave signal on pin 9
+//////////////////////////////////////////////////////////////////
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN            6
+#define fadePin 9
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      40
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-int delayval = 25; // delay for half a second
-
-//ENCODER SETUP 
-//these pins can not be changed 2/3 are special pins
-int encoderPin1 = 2;
-int encoderPin2 = 3;
-volatile int lastEncoded = 0;
-volatile long encoderValue = 0;
-long lastencoderValue = 0;
-int lastMSB = 0;
-int lastLSB = 0;
-
-void setup() {
-//  Setup Encoder as input
-  Serial.begin (9600);
-
-  pinMode(encoderPin1, INPUT); 
-  pinMode(encoderPin2, INPUT);
-
-  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
-  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
-
-  //call updateEncoder() when any high/low changed seen
-  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
-  attachInterrupt(0, updateEncoder, CHANGE); 
-  attachInterrupt(1, updateEncoder, CHANGE);  
-
-//  LED STRIP SETUP
-pixels.begin(); // This initializes the NeoPixel library.  
-  
-
+void setup(){
+  pinMode(fadePin, OUTPUT);  
 }
 
-void loop() {
- Serial.println(encoderValue); 
- int brightnessValue = map(encoderValue,0,100,0,255);
+void loop(){
 
- // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  
-  for(int i=0;i<NUMPIXELS;i++){
-    pixels.setBrightness(brightnessValue);
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(255,255,255)); // Moderately bright green color.
+  for(int i = 0; i<360; i++){
+    //convert 0-360 angle to radian (needed for sin function)
+    float rad = DEG_TO_RAD * i;
 
-    pixels.show(); // This sends the updated pixel color to the hardware.
+    //calculate sin of angle as number between 0 and 255
+    int sinOut = constrain((sin(rad) * 128) + 128, 0, 255); 
 
-//    delay(delayval); // Delay for a period of time (in milliseconds).
+    analogWrite(fadePin, sinOut);
 
+    delay(15);
   }
-  
+
 
 }
