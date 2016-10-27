5dea2475de0c1123c6fb5cef50066bfc8c768bd1
mark breneman <mark.breneman@gmail.com>
2016-10-27 15:23:15 -0400

    transistor with Encoder

--- Curve-ID-Lighting.ino
+++ Curve-ID-Lighting.ino
@@ -1,28 +1,38 @@
-//////////////////////////////////////////////////////////////////
-//©2011 bildr
-//Released under the MIT License - Please reuse change and share
-//Simple code to output a PWM sine wave signal on pin 9
-//////////////////////////////////////////////////////////////////
-
 #define fadePin 9
 
-void setup(){
-  pinMode(fadePin, OUTPUT);  
+//ENCODER SETUP
+//these pins can not be changed 2/3 are special pins
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+long lastencoderValue = 0;
+int lastMSB = 0;
+int lastLSB = 0;
+
+void setup() {
+
+  //  Setup Encoder as input
+  Serial.begin (9600);
+
+  pinMode(fadePin, OUTPUT);
+  pinMode(encoderPin1, INPUT);
+  pinMode(encoderPin2, INPUT);
+
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
 }
 
-void loop(){
-
-  for(int i = 0; i<360; i++){
-    //convert 0-360 angle to radian (needed for sin function)
-    float rad = DEG_TO_RAD * i;
-
-    //calculate sin of angle as number between 0 and 255
-    int sinOut = constrain((sin(rad) * 128) + 128, 0, 255); 
-
-    analogWrite(fadePin, sinOut);
-
-    delay(15);
-  }
+void loop() {
+  Serial.println(encoderValue);
+//  int brightnessValue = map(encoderValue, 0, 100, 0, 255);
+  analogWrite(fadePin,255);
+  
 
 
 }
